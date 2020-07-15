afdf46d85febec16f689c9a90b448caae5a408aa
Greg Martin <gregm31@live.com>
2020-07-16 09:34:19 +1000

    Added get profile route

---
+++ routes/api/profile.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const request = require("request")
+const config = require("config")
+const router = express.Router()
+const auth = require("../../middleware/auth")
+const { check, validationResult } = require("express-validator")
+
+const Profile = require("../../models/Profile")
+const User = require("../../models/User")
+
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"])
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" })
+    }
+
+    res.json(profile)
+  } catch (error) {
+    console.error(err.message)
+    res.status(500).send("Server Error")
+  }
+})
+
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
