a0c0e964261d99ce9f4f80d02bffdd1d380b2054
hai14894 <hohoanghai14894@gmail.com>
2020-07-20 13:37:02 +1000

    calling api

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,19 +1,23 @@
 import React, {useEffect, useState} from "react"
 import {API_URL, API_KEY} from '../Config'
+
+import styles from './Landing.module.css'
 import axios from "axios"
+import {Typography, Row} from 'antd'
+import MainImg from "../Sections/MainImg"
+const {Title} = Typography
+
 
 export const Landing = () => {
+  const [Movies, setMovies] = useState([])
 
   useEffect(() => {
-    // fetch(`${API_URL}movie/popular?api_key=${API_KEY}&languague=en-US&page=1`)
-    //   .then(res => res.json)
-    //   .then(res => {
-    //     console.log(res);
-    //   })
     async function getMovie(){
       try{
         const res = await axios.get(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
         console.log(res);
+        setMovies(res.results)
+
       } catch(err){
         console.log(err);
       }
@@ -21,10 +25,28 @@ export const Landing = () => {
     getMovie ()
   }, [])
   return (
-    <>
-           
-        <h1>Our Landing Page</h1>
-        <div>popular movie</div> 
-    </>
+  
+  <div style={{ width: '100%', margin:0 }}>
+          {/* Landing movie img */}
+    <MainImg/>
+    
+      {/* Body */}
+      <div className={styles.landingBody}>
+        <Title level={2}>Latest movies</Title>
+        <hr/>
+
+      
+        
+
+        {/* Load more button */}
+        <div className={styles.buttonWrapper}>
+          <button> 
+            LOAD MORE
+          </button>
+        </div>
+        
+      </div>
+    </div>
+  
   )
 }
