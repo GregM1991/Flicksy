58c5a1b94444649d7fa4d3806cdab4ec2e9a3334
hai14894 <hohoanghai14894@gmail.com>
2020-07-20 10:30:42 +1000

    use axios to fetch the movie from movie API

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,30 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import {API_URL, API_KEY} from '../Config'
+import axios from "axios"
 
 export const Landing = () => {
-  return <h1>Our Landing Page</h1>
+
+  useEffect(() => {
+    // fetch(`${API_URL}movie/popular?api_key=${API_KEY}&languague=en-US&page=1`)
+    //   .then(res => res.json)
+    //   .then(res => {
+    //     console.log(res);
+    //   })
+    async function getMovie(){
+      try{
+        const res = await axios.get(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+        console.log(res);
+      } catch(err){
+        console.log(err);
+      }
+    }
+    getMovie ()
+  }, [])
+  return (
+    <>
+           
+        <h1>Our Landing Page</h1>
+        <div>popular movie</div> 
+    </>
+  )
 }
