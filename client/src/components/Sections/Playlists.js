1dd6c8b3568b79cf81448df822bde9a8766fce1e
hai14894 <hohoanghai14894@gmail.com>
2020-07-23 15:45:06 +1000

    render the playlists

--- client/src/components/Sections/Playlists.js
+++ client/src/components/Sections/Playlists.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 const Playlists = () => {
+    const [playlists, setPlaylists] = useState([])
+    useEffect(() => {
+        
+        const getPlayLists = async () => {
+            const token = localStorage.getItem("token")
+            const response = await axios.get('api/profile/me', {headers : { "x-auth-token": token }})
+            setPlaylists(response.data.playlists) 
+            console.log(response.data.playlists[0].playlistname)
+        }
+        getPlayLists()
+    },[])
+    
+    
+    
     return (
         <div>
-            this is the playlists
+            <div>{playlists.map(playlist =>(
+                <li key={playlist.playlistname}>{playlist.playlistname}</li>
+            ))}</div>
         </div>
     )
 }
