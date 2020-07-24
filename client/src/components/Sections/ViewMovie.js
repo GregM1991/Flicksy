ed8ccaef5dc65f486f83c0bcbf9b8fc04ac8b7d0
hai14894 <hohoanghai14894@gmail.com>
2020-07-24 21:58:45 +1000

    refactor the code

--- client/src/components/Sections/ViewMovie.js
+++ client/src/components/Sections/ViewMovie.js
@@ -3,21 +3,25 @@ import axios from "axios"
 import { API_URL, API_KEY, IMG_URL } from "../Config"
 import UserContext from "../../context/UserContext"
 const ViewMovie = (props) => {
-  const { setUserData, setUserProfile, userProfile } = useContext(UserContext)
+  const { userProfile } = useContext(UserContext)
   const [getMovie, setGetMovie] = useState("")
+
+  const [Crews, setCrews] = useState([])
   const movieId = props.match.params.movieId
+  // async () =>{
+  //   const res = await axios.get(`${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`)
+  // console.log(res)
+  // } 
+  
+  async function getMovieDetails(){
+    const response = await axios.get(
+      `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    )
+    console.log(response)
+    setGetMovie(response.data)
+  }
   useEffect(() => {
-    try {
-      ;(async () => {
-        const response = await axios.get(
-          `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
-        )
-        console.log(response)
-        setGetMovie(response.data)
-      })()
-    } catch (e) {
-      console.log(e)
-    }
+    getMovieDetails()
   }, [movieId])
 
   return (
@@ -41,6 +45,11 @@ const ViewMovie = (props) => {
       <div>{getMovie.runtime} mins</div>
       <div>Release date: {getMovie.release_date}</div>
       <div>Rating: {getMovie.vote_average}</div>
+
+      <div>
+        <button>Show actors</button>
+      </div>
+
     </div>
   )
 }
