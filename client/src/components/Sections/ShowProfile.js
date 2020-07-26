34d990f0bef6b51cd895039ce3d6d97e51d057cf
Greg Martin <gregm31@live.com>
2020-07-27 09:25:31 +1000

    Added Create playlist component, simplified playlists component

--- client/src/components/Sections/ShowProfile.js
+++ client/src/components/Sections/ShowProfile.js
@@ -1,11 +1,24 @@
-import React from "react"
-import Playlists from "./Playlists"
+import React, { useContext } from "react"
+import SinglePlaylist from "./SinglePlaylist"
+import UserContext from "../../context/UserContext"
+import { useHistory } from "react-router-dom"
+// import Playlists from "./Playlists"
 
 export const ShowProfile = () => {
+  const history = useHistory()
+  const { userProfile } = useContext(UserContext)
+  const createPlaylist = () => {
+    history.push("/profile/create-playlist")
+  }
   return (
     <>
       <div>My Profile</div>
-      <Playlists />
+      <div>
+        {userProfile.playlists.map((playlist) => (
+          <SinglePlaylist key={playlist._id.toString()} />
+        ))}
+      </div>
+      <button onClick={createPlaylist}>Create Playlist</button>
     </>
   )
 }
