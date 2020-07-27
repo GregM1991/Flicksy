ad8b7444a3f405bc5237d114516e9ec3a2db94e3
hai14894 <hohoanghai14894@gmail.com>
2020-07-27 23:00:59 +1000

    successfully pass the data from the form to the API call

--- client/src/components/Sections/SearchMovie.js
+++ client/src/components/Sections/SearchMovie.js
@@ -1,13 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from "axios"
+import { API_URL, API_KEY } from "../Config"
 
 
 
 const SearchMovie = () => {
+   
     const [formData, setFormData] = useState("")
+    const [moviesData, setMoviesData] = useState("")
+    //when submitted it's gonna fetch the formData
+    const setOnSubmit = (e) =>{
+        e.preventDefault()
+
+        console.log('submited');
+        // function that take the formData and put it in the query params
+        async function getMovies() {
+            const response = await axios.get(
+              `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${formData}&page=1`)
+            console.log(response);
+            setMoviesData(response)
+          }
+        //when the submit button is clicked, getMovie function get called
+        getMovies()
+    
+    }
+    
+
+    
     return (
         <div>
-            <form onSubmit="">
+            <form onSubmit={setOnSubmit}>
                 <input 
                     type="text" 
                     placeholder="search movie by title" 
