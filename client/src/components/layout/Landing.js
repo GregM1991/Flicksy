20a20fae9a6c63f03d47f9d8c41ae2e7b9196fe4
hai14894 <hohoanghai14894@gmail.com>
2020-07-29 14:55:25 +1000

    add top Rated movies to landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,37 +5,66 @@ import SingleMovie from "../Sections/SingleMovie"
 import SearchMovie from "../Sections/SearchMovie"
 
 export const Landing = () => {
+
+
+
   const [movies, setMovies] = useState([])
+
+  const [topRated, setTopRated] = useState([])
+
+  const [currentPage, setCurrentPage] = useState(1)
+
+
 
   const [Searched, setSearched] = useState(false)
   //the function that change the state of Searched
   function hasSearched(){
     setSearched(true)
   }
-
-  useEffect(() => {
-    async function getMovies() {
-      try {
-        const res = await axios.get(
-          `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-        )
-        setMovies(res.data.results)
-        console.log(res.data.results)
-      } catch (err) {
-        console.log(err)
-      }
+  async function getMovies() {
+    try {
+      const res = await axios.get(
+        `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage}`
+      )
+      setMovies(res.data.results)
+      console.log(res.data.results)
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    getMovies()
+  async function getTopRatedMovies() {
+    try {
+      const res = await axios.get(
+        `${API_URL}movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+      )
+      setTopRated(res.data.results)
+      console.log(res.data.results)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  
+  useEffect(() => {
+    
+  getTopRatedMovies()
+
   }, [])
+  useEffect(() => {
+    getMovies()
+  },[currentPage])
+
+  
 
   return (
     <>
     {/* pass the hasSearched function to the SearchMovie component */}
     <SearchMovie hasSearched={hasSearched}/>  
+    <h1>Popular Movies</h1>
     {/* if user searched something, the default movies wont get rendered on the landing page */}
       {!Searched && movies.map((movie) => {
         return (
+        
           <SingleMovie
             key={movie.id}
             image={`${IMG_URL}w200${movie.poster_path}`}
@@ -45,6 +74,24 @@ export const Landing = () => {
           />
         )
       })}
+
+    
+
+    <h1>Top Rated Movies</h1>
+    {!Searched && topRated.map((movie) => {
+        return (
+        
+          <SingleMovie
+            key={movie.id}
+            image={`${IMG_URL}w200${movie.poster_path}`}
+            title={movie.original_title}
+            text={movie.overview}
+            movieId={movie.id}
+          />
+        )
+      })}
+      
+     
     </>
   )
 }
